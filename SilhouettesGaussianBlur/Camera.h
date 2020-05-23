3811fd27ca4da3fb881309bace251f70b37384fc
ItsAbdula <43587099+ItsAbdula@users.noreply.github.com>
2020-05-23 12:05:07 +0900

    remove comments

--- SilhouettesGaussianBlur/Camera.h
+++ SilhouettesGaussianBlur/Camera.h
@@ -3,15 +3,14 @@
 
 #include "Transform.h"
 
-// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
-enum Camera_Movement {
+enum Camera_Movement
+{
     FORWARD,
     BACKWARD,
     LEFT,
     RIGHT
 };
 
-// Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
 const float SPEED = 10.0f;
