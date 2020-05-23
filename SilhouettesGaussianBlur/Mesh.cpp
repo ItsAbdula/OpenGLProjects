af11719aa3aed428a50b6a70272c2b157e97c133
ItsAbdula <43587099+ItsAbdula@users.noreply.github.com>
2020-05-23 20:55:23 +0900

    make struct for vertexData(position, normal, tex). easy for allocate vram

--- SilhouettesGaussianBlur/Mesh.cpp
+++ SilhouettesGaussianBlur/Mesh.cpp
@@ -9,7 +9,7 @@ Mesh::Mesh(MeshData *_meshData)
     VAO = allocateVAO();
     {
         glBindVertexArray(VAO);
-        VBOs.push_back(allocateVBO(0, _meshData->getVertices()));
+        VBOs.push_back(allocateVBO(_meshData->getVertexData()));
         glBindVertexArray(0);
     }
 
@@ -65,13 +65,37 @@ GLuint allocateEBO(vector<GLuint> indices)
     return EBO;
 }
 
-GLuint allocateVBO(const GLuint attribIndex, vector<glm::vec3> *vertexData)
+GLuint allocateVBO(vector<VertexData>& vertexData)
 {
     GLuint VBOIndex = 0;
     glGenBuffers(1, &VBOIndex);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBOIndex);
-    glBufferData(GL_ARRAY_BUFFER, vertexData->size() * sizeof(glm::vec3), &(vertexData->front()), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertexData.size() * sizeof(VertexData), &(vertexData.front()), GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, normal));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, texCoord));
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    return VBOIndex;
+}
+
+GLuint allocateVBO(const GLuint attribIndex, vector<glm::vec3>& vertexData)
+{
+    GLuint VBOIndex = 0;
+    glGenBuffers(1, &VBOIndex);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBOIndex);
+    glBufferData(GL_ARRAY_BUFFER, vertexData.size() * sizeof(glm::vec3), &(vertexData.front()), GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(attribIndex);
     glVertexAttribPointer(attribIndex, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
@@ -114,14 +138,19 @@ MeshData::MeshData(const aiScene* _scene)
     if (_scene == NULL) return;
     if (_scene->HasMeshes() == false) return;
 
-    vertices = copyVertices(_scene);
+    vertexData = copyVertexData(_scene);
+
+    vertexPositions = copyVertexPositions(_scene);
+    vertexNormals = copyVertexNormals(_scene);
+    vertexTextureCoords = copyVertexTextureCoords(_scene);
+
     faces = copyFaces(_scene);
     indices = copyIndices(_scene);
 }
 
 const GLuint MeshData::countVertices()
 {
-    return vertices.size();
+    return vertexPositions.size();
 }
 
 const GLuint MeshData::countFaces()
@@ -134,7 +163,33 @@ const GLuint MeshData::countIndices()
     return indices.size();
 }
 
-vector<glm::vec3> MeshData::copyVertices(const aiScene* pScene)
+vector<VertexData>& MeshData::getVertexData()
+{
+    return vertexData;
+}
+
+vector<VertexData> MeshData::copyVertexData(const aiScene* pScene)
+{
+    vector<VertexData> vertexData;
+    for (unsigned int i = 0; i < pScene->mNumMeshes; i++)
+    {
+        auto mesh = pScene->mMeshes[i];
+
+        VertexData vertexDatum;
+        for (unsigned int j = 0; j < mesh->mNumVertices; j++)
+        {
+            vertexDatum.position = glm::vec3(mesh->mVertices[j].x, mesh->mVertices[j].y, mesh->mVertices[j].z);
+            vertexDatum.normal = glm::vec3(mesh->mNormals[j].x, mesh->mNormals[j].y, mesh->mNormals[j].z);
+            vertexDatum.texCoord = glm::vec2(mesh->mTextureCoords[0][j].x, mesh->mTextureCoords[0][j].y);
+
+            vertexData.push_back(vertexDatum);
+        }
+    }
+
+    return vertexData;
+}
+
+vector<glm::vec3> MeshData::copyVertexPositions(const aiScene* pScene)
 {
     vector<glm::vec3> vertices;
     for (unsigned int i = 0; i < pScene->mNumMeshes; i++)
@@ -147,6 +202,40 @@ vector<glm::vec3> MeshData::copyVertices(const aiScene* pScene)
     }
 
     return vertices;
+}
+
+vector<glm::vec3> MeshData::copyVertexNormals(const aiScene * pScene)
+{
+    vector<glm::vec3> normals;
+    for (unsigned int i = 0; i < pScene->mNumMeshes; i++)
+    {
+        auto mesh = pScene->mMeshes[i];
+        if (mesh->HasNormals() == false) return normals;
+
+        for (unsigned int j = 0; j < mesh->mNumVertices; j++)
+        {
+            normals.push_back(glm::vec3(mesh->mNormals[j].x, mesh->mNormals[j].y, mesh->mNormals[j].z));
+        }
+    }
+
+    return normals;
+}
+
+vector<glm::vec2> MeshData::copyVertexTextureCoords(const aiScene * pScene)
+{
+    vector<glm::vec2> texCoords;
+    for (unsigned int i = 0; i < pScene->mNumMeshes; i++)
+    {
+        auto mesh = pScene->mMeshes[i];
+        if (mesh->HasTextureCoords(0) == false) return texCoords;
+
+        for (unsigned int j = 0; j < mesh->mNumVertices; j++)
+        {
+            texCoords.push_back(glm::vec2(mesh->mTextureCoords[0][j].x, mesh->mTextureCoords[0][j].y));
+        }
+    }
+
+    return texCoords;
 }
 
 vector<vector<GLuint>> MeshData::copyFaces(const aiScene* pScene)
@@ -191,14 +280,24 @@ vector<GLuint> MeshData::copyIndices(const aiScene* pScene)
     return indices;
 }
 
-vector<glm::vec3>* MeshData::getVertices()
+glm::vec3 *MeshData::getVertexPosition(GLuint vertexIndex)
 {
-    return &vertices;
+    return &(vertexPositions.at(vertexIndex));
 }
 
-glm::vec3 *MeshData::getVertex(GLuint vertexIndex)
+vector<glm::vec3>& MeshData::getVertexPositions()
 {
-    return &(vertices.at(vertexIndex));
+    return vertexPositions;
+}
+
+vector<glm::vec3>& MeshData::getVertexNormals()
+{
+    return vertexNormals;
+}
+
+vector<glm::vec2>& MeshData::getVertexTextureCoords()
+{
+    return vertexTextureCoords;
 }
 
 vector<GLuint>& MeshData::getIndices()
@@ -216,7 +315,7 @@ vector<const glm::vec3*> MeshData::getFaceVertices(GLuint faceIndex)
     vector<const glm::vec3*> result;
     for (auto vertexIndex : *getFace(faceIndex))
     {
-        result.push_back(getVertex(vertexIndex));
+        result.push_back(getVertexPosition(vertexIndex));
     }
 
     return result;
--- SilhouettesGaussianBlur/Mesh.h
+++ SilhouettesGaussianBlur/Mesh.h
@@ -10,6 +10,13 @@
 
 using std::vector;
 
+struct VertexData
+{
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texCoord;
+};
+
 class MeshData
 {
 public:
@@ -19,18 +26,30 @@ public:
     const GLuint countFaces();
     const GLuint countIndices();
 
-    vector<glm::vec3> *getVertices();
+    vector<VertexData>& getVertexData();
+
+    vector<glm::vec3>& getVertexPositions();
+    vector<glm::vec3>& getVertexNormals();
+    vector<glm::vec2>& getVertexTextureCoords();
     vector<GLuint>& getIndices();
-    glm::vec3 *getVertex(GLuint vertexIndex);
+    glm::vec3 *getVertexPosition(GLuint vertexIndex);
     vector<GLuint> *getFace(GLuint faceIndex);
     vector<const glm::vec3*> getFaceVertices(GLuint faceIndex);
 
 private:
-    vector<glm::vec3> vertices;
+    vector<VertexData> vertexData;
+
+    vector<glm::vec3> vertexPositions;
+    vector<glm::vec3> vertexNormals;
+    vector<glm::vec2> vertexTextureCoords;
     vector<vector<GLuint>> faces;
     vector<GLuint> indices;
 
-    vector<glm::vec3> copyVertices(const aiScene* pScene);
+    vector<VertexData> copyVertexData(const aiScene * pScene);
+
+    vector<glm::vec3> copyVertexPositions(const aiScene* pScene);
+    vector<glm::vec3> copyVertexNormals(const aiScene* pScene);
+    vector<glm::vec2> copyVertexTextureCoords(const aiScene* pScene);
     vector<vector<GLuint>> copyFaces(const aiScene* pScene);
     vector<GLuint> copyIndices(const aiScene* pScene);
 };
@@ -55,7 +74,8 @@ private:
     vector<GLuint> VBOs;
     GLuint EBO;
 };
-GLuint allocateVBO(const GLuint attribIndex, vector<glm::vec3> *VBO);
+GLuint allocateVBO(const GLuint attribIndex, vector<glm::vec3>& vertexData);
+GLuint allocateVBO(vector<VertexData>& vertexData);
 GLuint allocateVAO();
 GLuint allocateEBO(vector<GLuint> indices);
 
